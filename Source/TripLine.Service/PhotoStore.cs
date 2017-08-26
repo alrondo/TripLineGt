9105410a54b89452498b8494d1caab9da03409cd
alrondo <alrondo@hotmail.com>
2017-08-25 22:04:41 -0400

    ignore unnescessary files

--- Source/TripLine.Service/PhotoStore.cs
+++ Source/TripLine.Service/PhotoStore.cs
@@ -13,7 +13,7 @@ namespace TripLine.Service
 {
     public class PhotoStore
     {
-        private readonly LocalFileFolders _fileFolder;
+        private readonly LocalFileFolders _fileFolder=null;
 
         private readonly LocationService _locationService;
 
