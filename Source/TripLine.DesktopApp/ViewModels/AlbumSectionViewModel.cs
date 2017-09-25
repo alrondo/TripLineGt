d88b16b2063779b4d41dd7fad7773550b1d416ea
alrondo <alrondo@hotmail.com>
2017-09-25 17:04:47 -0400

    Improved main look

---
+++ Source/TripLine.DesktopApp/ViewModels/AlbumSectionViewModel.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TripLine.DesktopApp.ViewModels
+{
+    public class AlbumSectionViewModel : BaseViewModel
+    {
+        public string DisplayName { get; set; }
+
+        // Little trick to disable listbox selection...   We bind Listbox selectitems to NoSelection.  
+        // The real selections are handled by the checkboxes inside the list items.
+        public AlbumItemViewModel NoSelection
+        {
+            get { return null; }   // no selection
+            set
+            {
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(SelectedItem));
+            }
+        }
+
+        private List<AlbumItemViewModel> _items;
+        public List<AlbumItemViewModel> Items
+        {
+            get { return _items; }
+            set
+            {
+                if (value == _items)
+                    return;
+
+                _items = value;
+                OnPropertyChanged();
+
+                SelectedItem = _items?.First();
+            }
+        }
+        private AlbumItemViewModel _selectedItem;
+        public AlbumItemViewModel SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                if (value == _selectedItem)
+                    return;
+
+                _selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+        public AlbumSectionViewModel( ) : base("AlbumSection")
+        {
+            Items = Items;
+        }
+    }
+}
